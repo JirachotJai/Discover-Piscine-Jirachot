d60b758d359f8d15919f430614c82dd18f6e63d9
jaijirachot@gmail.com <jaijirachot@gmail.com>
2026-02-12 22:40:33 +0700

    fix 5.5

--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) == 1:
+if len(sys.argv) <= 1:
     print("none")
 else:
-    for i in sys.argv[1:]:
-        print(i.upper(), end= " ")
+    print(sys.argv[1])
